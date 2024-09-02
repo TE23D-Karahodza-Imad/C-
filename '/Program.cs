5d0173a54dcc2098e90d798c805b4e64b27d8a1b
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2024-09-02 09:37:34 +0200

    september

--- '/Program.cs
+++ '/Program.cs
@@ -1,2 +1,23 @@
-﻿Console.WriteLine("And in the touch of silence.. drn drn and in naked light i saw.");
-hej
+﻿Console.WriteLine("Användarnamn: ");
+string name = Console.ReadLine();
+
+if (name.ToLower() == "imad")
+{
+Console.WriteLine("Inloggad! Välkommen!");
+}
+
+else if (name == "Adam")
+{
+Console.WriteLine("Du får inte komma in!");
+}
+else if (name == "Dahri")
+{
+    Console.WriteLine("Tagga härifrån");
+}
+
+else 
+{
+    Console.WriteLine("DU är stupid!");
+}
+
+Console.ReadLine();
