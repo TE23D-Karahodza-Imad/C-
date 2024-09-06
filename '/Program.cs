8adad8e1bf0bf5d0b319c79d431a873a6e3409d2
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2024-09-06 10:52:07 +0200

    You died

--- '/Program.cs
+++ '/Program.cs
@@ -1,23 +1,59 @@
-﻿Console.WriteLine("Användarnamn: ");
-string name = Console.ReadLine();
-
-if (name.ToLower() == "imad")
+﻿Console.WriteLine("Hello adventurer, you have entered the Redlight Forest.. You have two paths to choose, left or right? ");
+//
+Console.Write("> ");
+string pathChoice = Console.ReadLine();
+//
+if (pathChoice.ToLower() == "right")
 {
-Console.WriteLine("Inloggad! Välkommen!");
+    Console.WriteLine("As you walk along the dirt pathway, surrounded by the Red trees.. Something crosses your path.");
+    Console.WriteLine("You are greeted by a small Gnome, offering you some mushrooms.. do you accept or reject them? ");
+
+    //Choice
+    Console.Write("> ");
+    string mushroomChoice = Console.ReadLine();
+
+    if (mushroomChoice.ToLower() == "accept");
+    {
+        Console.WriteLine("...");
+        Console.WriteLine("Where am I?.. you ask yourself.");
+        Console.WriteLine("You find yourself lying down in a wet muddy pond in the middle of a Village..");
+        Console.WriteLine("But it is not any village, it is a Gnome Village.. Do you ask for help or leave?");  
+
+        //Choice
+        Console.Write("> ");
+        string helpleaveChoice = Console.ReadLine();
+
+        if (helpleaveChoice.ToLower() == "help");
+        {
+            Console.WriteLine("Your wet and weak body gets made fun of by the Gnomes");
+            Console.WriteLine("Gnomes take you in to their tavern where you are cooked and eaten alive by them..");
+            Console.WriteLine("You Died..");
+        }
+    }
+
+    else if
+    {
+
+    }
+
+    else
+    {
+
+    }
+
 }
 
-else if (name == "Adam")
-{
-Console.WriteLine("Du får inte komma in!");
-}
-else if (name == "Dahri")
-{
-    Console.WriteLine("Tagga härifrån");
-}
 
-else 
-{
-    Console.WriteLine("DU är stupid!");
-}
 
-Console.ReadLine();
+
+
+
+ {
+if (pathChoice.ToLower() == "left")
+ }
+
+
+
+
+    //
+    Console.ReadLine();
